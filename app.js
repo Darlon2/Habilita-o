5b5e1c22aadb6ae66bfba92829b432608ed0de9d
Darlon Oliveira <darlon.osilva@gmail.com>
2025-02-19 21:58:36 -0300

    campo vazio ao digitar idade

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 
 alert("Boas vindas ao nosso Site!");
 
+let nome = 'Lua' 
+let idade = 25
 
-let nome = prompt ("Digite seu nome: ");
-let idade = prompt ("Digite sua idade: ");
 
-//alert("Olá, " + nome + "! Você tem " + idade + " anos.");
+let nomeUsuario = prompt ("Digite seu nome: ");
+let suaIdade = prompt ("Digite sua idade: ");
+
+
 let numeroDeVendas = 50
 let saldoDisponivel = 1000
-//let idade = 25
-//let nome = "Lua"
 
 
-if (nome == "" || idade == "") {
-    let mensagemDeErro = document.createElement( )
-    mensagemDeErro ("Erro! Preencha todos os campos") 
-alert(mensagemDeErro);
-}
 
-if (idade >= 18) {
-    alert("Ótimo " + nome + "! Você tem " + idade + " anos. já pode Tirar sua Habilitação.");
-}
-
-else {
-    alert("Poxa, que pena  " + nome + "! Você não pode Tirar a Habilitação, você tem apenas " + idade + " anos. .");
+if (suaIdade === null || suaIdade.trim() === "" || isNaN(suaIdade)) {
+    alert("Erro: Por favor, digite um número válido.");
+} else {
+    const suaIdadedadeNum = Number(suaIdade);
+    
+    if (suaIdadedadeNum >= 18) {
+        alert("Ótimo " + nomeUsuario + "! Você tem " + suaIdade + " anos. já pode Tirar sua Habilitação.");
+    } else {
+        alert("Poxa, que pena  " + nomeUsuario + "! Você não pode Tirar a Habilitação, você tem apenas " + suaIdade + " anos. .");
+    }
 }
