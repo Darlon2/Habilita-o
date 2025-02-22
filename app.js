03339260b4aa587844cf641a249d5e362d5f8caf
Darlon Oliveira <darlon.osilva@gmail.com>
2025-02-22 18:04:00 -0300

    retirada de let

--- app.js
+++ app.js
@@ -3,16 +3,8 @@ alert("Boas vindas ao nosso Site!");
 
 let nome = 'Lua' 
 let idade = 25
-
-
-let nomeUsuario = prompt ("Digite seu nome: ");
-let suaIdade = prompt ("Digite sua idade: ");
-
-
-let numeroDeVendas = 50
-let saldoDisponivel = 1000
-
-
+nomeUsuario = prompt ("Digite seu nome: ");
+suaIdade = prompt ("Digite sua idade: ");
 
 if (suaIdade === null || suaIdade.trim() === "" || isNaN(suaIdade)) {
     alert("Erro: Por favor, digite um número válido.");
@@ -25,3 +17,11 @@ if (suaIdade === null || suaIdade.trim() === "" || isNaN(suaIdade)) {
         alert("Poxa, que pena  " + nomeUsuario + "! Você não pode Tirar a Habilitação, você tem apenas " + suaIdade + " anos. .");
     }
 }
+
+
+
+
+
+
+let numeroDeVendas = 50
+let saldoDisponivel = 1000
